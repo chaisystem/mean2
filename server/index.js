4fb1796f19dde32b502a4341c9b2b00570a6b1a7
chaisystem <isoto@netwarmonitor.com>
2019-05-02 12:56:45 -0500

    Config app
    ++

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000,()=>{
+// settings
+app.set('port', process.env.PORT || 3000); // puerto asignado por el sistema o puerto 3000
+
+// middlewares
+
+//routes
+
+//starting the serve
+
+app.listen(app.get('port'),()=>{
     console.log('server on port 3000')
 })
