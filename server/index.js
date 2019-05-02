6113e270b26b11d0c7740c759a93c5804508d2e4
chaisystem <isoto@netwarmonitor.com>
2019-05-02 13:56:16 -0500

    ident

--- server/index.js
+++ server/index.js
@@ -5,17 +5,17 @@ const app = express();
 const { mongoose } = require('./database');
 
 // settings
-app.set('port', process.env.PORT || 3000); // puerto asignado por el sistema o puerto 3000
+    app.set('port', process.env.PORT || 3000); // puerto asignado por el sistema o puerto 3000
 
 // middlewares
-app.use(morgan('dev')); // muestra informacion de la peticion
-app.use(express.json()); // es para que el servidor pueda interpretar json
+    app.use(morgan('dev')); // muestra informacion de la peticion
+    app.use(express.json()); // es para que el servidor pueda interpretar json
 
 //routes
-app.use(require('./routes/employee.routes'));
+    // app.use(require('./routes/employee.routes')); 
+    app.use('/api/employees',require('./routes/employee.routes')); // <- cambiamos la ruta
 
 //starting the serve
-
-app.listen(app.get('port'),()=>{
-    console.log('server on port ',app.get('port'))
-})
+    app.listen(app.get('port'),()=>{
+        console.log('server on port ',app.get('port'))
+    })
